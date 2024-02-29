fe2681f5b715ec8909a882c819e69f06a4818fc5
pnywise <bunayya12345@gmail.com>
2024-02-29 21:46:42 +0700

    init

---
+++ cmd/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	chat_server "github.com/PNYwise/chat-server/proto"
+	"google.golang.org/grpc"
+)
+
+type ChatServer struct {
+	clients      map[string]chat_server.Broadcast_CreateStreamServer
+	clientsLock  sync.Mutex
+	messageQueue chan *chat_server.Message
+	chat_server.UnimplementedBroadcastServer
+}
+
+func (c *ChatServer) CreateStream(connect *chat_server.Connect, stream chat_server.Broadcast_CreateStreamServer) error {
+	clientID := connect.GetId()
+
+	c.clientsLock.Lock()
+	c.clients[clientID] = stream
+	c.clientsLock.Unlock()
+
+	go func() {
+		for {
+			select {
+			case msg := <-c.messageQueue:
+				if err := c.sendMessageToClient(msg.GetTo(), msg); err != nil {
+					log.Printf("Error sending queued message to client %s: %v", msg.GetTo(), err)
+				}
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (s *ChatServer) sendMessageToClient(clientID string, msg *chat_server.Message) error {
+	s.clientsLock.Lock()
+	defer s.clientsLock.Unlock()
+	clientStream, ok := s.clients[clientID]
+	fmt.Printf("%v", msg)
+	if !ok {
+		return nil
+	}
+
+	if err := clientStream.Send(msg); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *ChatServer) BroadcastMessage(ctx context.Context, message *chat_server.Message) (*chat_server.Close, error) {
+	fmt.Printf("%v", message)
+	c.messageQueue = make(chan *chat_server.Message)
+	return &chat_server.Close{}, nil
+}
+
+func main() {
+	// Inisialisasi server gRPC
+	server := grpc.NewServer()
+
+	// Menginisialisasi struktur ChatServer
+	chatServer := &ChatServer{
+		clients:      make(map[string]chat_server.Broadcast_CreateStreamServer),
+		messageQueue: make(chan *chat_server.Message),
+	}
+
+	// Mendaftarkan layanan obrolan ke server gRPC
+	chat_server.RegisterBroadcastServer(server, chatServer)
+
+	// Mulai mendengarkan di port yang ditentukan
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	defer listener.Close()
+
+	log.Println("Server listening on port :50051")
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
---
+++ scripts/grpc.sh
@@ -0,0 +1,5 @@
+protoc --proto_path=proto \
+    --go_out=proto --go_opt=paths=source_relative \
+    --go-grpc_out=proto \
+    --go-grpc_opt=paths=source_relative \
+    proto/chat.proto 
