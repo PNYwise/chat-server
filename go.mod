3ecb0ef4790bae9f586db8d53f1b3fad0ec3e4d9
pnywise <bunayya12345@gmail.com>
2024-03-02 00:13:21 +0700

    add repo

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/PNYwise/chat-server
 go 1.20
 
 require (
+	github.com/golang/protobuf v1.5.3
 	github.com/mattn/go-sqlite3 v1.14.22
 	google.golang.org/grpc v1.62.0
 	google.golang.org/protobuf v1.32.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
---
+++ internal/repository.go
@@ -0,0 +1,28 @@
+package internal
+
+import "database/sql"
+
+type User struct {
+	Id        uint
+	Name      string
+	CreatedAt sql.NullTime
+}
+
+type Massage struct {
+	Id        uint
+	Form      User
+	To        User
+	Content   string
+	CreatedAt sql.NullTime
+}
+
+type IUserRepository interface {
+	Cerate(user *User) error
+	Exist(id int) bool
+}
+
+type IMassageRepository interface {
+	Cerate(user *Massage) error
+	ReadByUserId(userId uint) (*[]Massage, error)
+	Delete(id uint) error
+}
